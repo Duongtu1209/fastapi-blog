05b5975688fe788aa53402c38efbd834443196e0
Duong Tu <tudv@operyn.ai>
2026-04-13 22:28:26 +0700

    create layout && demo homepage

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from fastapi-blog!")
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-if __name__ == "__main__":
-    main()
+@app.get("/", include_in_schema=False, name='home')
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"request": request, "posts": []})
